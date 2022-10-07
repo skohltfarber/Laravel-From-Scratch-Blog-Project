a0115141c0809a846b2d77cfd83438fa8ed106d6
Shawn Kohltfarber <skohltfarber@whytespyder.com>
2022-10-07 11:53:42 -0500

    V-47 #comment Failed Valdation and Old Input recovered.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,8 +17,8 @@ class RegisterController extends Controller
         // create the user
         $attributes = request()->validate([
             'name' => ['required','max:255'],
-            'username' => ['required','max:255','min:3'],
-            'email' => ['required','email','max:255'],
+            'username' => ['required','min:3','max:255', Rule::unique('users','username')],
+            'email' => ['required','email','max:255', Rule::unique('users','email')],
             'password' => ['required','min:8','max:255']
         ]);
 
