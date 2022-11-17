eb92dc760d5137fe8f211fb4f555131c7f897ec0
Shawn Kohltfarber <skohltfarber@whytespyder>
2022-11-17 11:51:14 -0600

    V-48 #comment show a success flash message.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,16 +17,23 @@ class RegisterController extends Controller
     public function store() {
 
         // create the user
+        // $attributes = request()->validate([
+        //     'name' => ['required','max:255'],
+        //     'username' => ['required','min:3','max:255', Rule::unique('users','username')],
+        //     'email' => ['required','email','max:255', Rule::unique('users','email')],
+        //     'password' => ['required','min:8','max:255']
+        // ]);
+
         $attributes = request()->validate([
-            'name' => ['required','max:255'],
-            'username' => ['required','min:3','max:255', Rule::unique('users','username')],
-            'email' => ['required','email','max:255', Rule::unique('users','email')],
-            'password' => ['required','min:8','max:255']
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:7|max:255',
         ]);
 
         User::create($attributes);
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
 
     }
 }
